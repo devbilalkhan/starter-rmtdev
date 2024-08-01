335ac12555a26557706f08a0498ae682eb838b80
Bilal Khan <bilal.khan@live.com>
2024-08-01 16:33:03 +1000

    Initial commit job board

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,17 @@
+import JobListItem from "./JobListItem";
+import PaginationControls from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SortingControl from "./SortingControls";
+
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__top"></div>
+      <div className="sidebar__top">
+        <ResultsCount />
+        <SortingControl />
+      </div>
+      <JobListItem />
+      <PaginationControls />
     </div>
   );
 }
